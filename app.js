b49d315e0522b0ccf4286b32cc05ae29a1a64d44
kigure <kakaloto0824@gmail.com>
2023-03-08 14:20:01 +0900

    header completed

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-$(function() {
+$(function(){
+  $(".contact").hover(
+    //マウスオーバー時の処理
+    function() {
+      $(this).css("opacity", 0.5);
+    },
+    //マウスアウト時の処理
+    function() {
+      $(this).css("opacity", 1.0);
+    }
+  );
 
-
-
+  
 });
