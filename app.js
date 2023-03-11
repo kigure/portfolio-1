a02ca5761b09c0f444ce799983d431874e4799b4
kigure <kakaloto0824@gmail.com>
2023-03-12 02:33:22 +0900

    fix layout

--- app.js
+++ app.js
@@ -9,7 +9,32 @@ $(function(){
       $(this).css("opacity", 1.0);
     }
   );
+// マウスが乗ったら背景色変更
+  $('#NEWS').hover(function() {
+    $(this).css("background-color", "#f0f0f0");
+    }, function() {
+    $(this).css("background-color", "white");
+  });
 
+  $('#ABOUT').hover(function() {
+    $(this).css("background-color", "#f0f0f0");
+    }, function() {
+    $(this).css("background-color", "white");
+  });
+
+  $('#BUSINESS').hover(function() {
+    $(this).css("background-color", "#f0f0f0");
+    }, function() {
+    $(this).css("background-color", "white");
+  });
+
+  $('#COMPANY').hover(function() {
+    $(this).css("background-color", "#f0f0f0");
+    }, function() {
+    $(this).css("background-color", "white");
+  });
+
+  // マウスをクリックしたら選択した要素にスクロール
   $('#NEWS').on('click',function(){
     const newsTop = $('#news').offset().top;
     $("html").animate({scrollTop: newsTop});
@@ -29,6 +54,24 @@ $(function(){
     const companyTop = $('#company').offset().top;
     $("html").animate({scrollTop: companyTop});
   });
+  
+  var btn = $('.button-top');
+  
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).on('load scroll', function(){
+    if($(this).scrollTop() > 200) {
+      btn.addClass('active');
+    }else{
+      btn.removeClass('active');
+    }
+  });
 
+  //スクロールしてトップへ戻る
+  btn.on('click',function () {
+    $('body,html').animate({
+      scrollTop: 0
+  });
+
+  });
 
 });
